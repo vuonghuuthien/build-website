2cc3beba6ffdb60eede2fc328b0ed41f8ef7a4a2
vuonghuuthien <thien.vuong@millennia.software>
2022-01-24 01:29:33 +0700

    Add list scss + Add font + Create Header Component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public logoName: string = 'APOLLO';
+  public logoNameEl: any;
+  // public 
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.setLogoName();
   }
 
+  setLogoName() {
+    this.logoNameEl = document.getElementById('logo-name');
+    for (let i = 0; i < this.logoName.length; i++) {
+      let spanEl: HTMLElement = document.createElement('div');
+      spanEl.innerText = this.logoName[i];
+      if (i != this.logoName.length - 1)
+        spanEl.style.marginRight = '5px';
+      this.logoNameEl?.appendChild(spanEl);
+    }
+  }
 }
